21d12051e4331666300a48d664ed90ab933469fb
ENOCH ABIODUN <G00380316@atu.ie>
2024-08-04 15:23:14 +0100

    changed back dir specification

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 # My Preferred Folders
-mkdir -p ./Documents ./Videos ./Coding/Projects
+mkdir -p  ~/Documents ~/Videos ~/Coding/Projects
 
 # Update the system
 echo "Updating system..."
@@ -16,6 +16,19 @@ sudo pacman -Syu --noconfirm
 # Development Tools
 echo "Installing basic development tools..."
 sudo pacman -S --noconfirm base-devel curl git wget unzip lazygit gcc
+
+# Clone and install yay
+cd /tmp
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si --noconfirm
+
+# Install necessary applications using yay
+yay -S --noconfirm postman-bin obsidian
+
+# Install Postman via Flatpak as a fallback
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install -y flathub com.getpostman.Postman
 
 # Clone Neovim Configuration Repository
 echo "Cloning Neovim configuration..."
@@ -40,7 +53,7 @@ echo -e '\n# Pyenv configuration' >> ~/.bashrc
 echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init --path)"' >> ~/.bashrc
 echo 'eval "$(pyenv init -)"' >> ~/.bashrc
-source ./.bashrc
+source ~/.bashrc
 pyenv install 3.11.4
 pyenv global 3.11.4
 pip install -U hyfetch
@@ -50,11 +63,12 @@ echo "Installing SDKMAN! and Java..."
 curl -s "https://get.sdkman.io" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 sdk install java
+java -version
 
 # Install Node.js using nvm
 echo "Installing nvm and Node.js..."
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-source ./.nvm/nvm.sh
+source ~/.nvm/nvm.sh
 nvm install node
 nvm use node
 
@@ -63,7 +77,7 @@ echo "Installing rbenv and Ruby..."
 curl -fsSL https://github.com/rbenv/rbenv-installer/raw/main/bin/rbenv-installer | bash
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-source ./.bashrc
+source ~/.bashrc
 rbenv install 3.1.2
 rbenv global 3.1.2
 
@@ -72,12 +86,13 @@ echo "Installing Go..."
 wget https://golang.org/dl/go1.20.5.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.20.5.linux-amd64.tar.gz
 echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
-source ./.bashrc
+source ~/.bashrc
 
 # Install Rust
 echo "Installing Rust..."
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-source $HOME/.cargo/env
+source ~/.cargo/env
+source ~/.bashrc
 rustup update
 
 # Install PHP and Lua
@@ -91,7 +106,7 @@ echo "Installing JetBrains Nerd Font..."
 echo "Downloading JetBrains Nerd Font..."
 FONT_ZIP="JetBrainsMono.zip"
 FONT_URL="https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.zip"
-cd ./Downloads
+cd ~/Downloads
 curl -LO "$FONT_URL"
 
 # Step 2: Extract the Font
@@ -100,8 +115,8 @@ unzip "$FONT_ZIP" -d JetBrainsMono
 
 # Step 3: Install the Font
 echo "Installing the font..."
-mkdir -p ./.local/share/fonts
-mv JetBrainsMono/* ./.local/share/fonts/
+mkdir -p ~/.local/share/fonts
+mv JetBrainsMono/* ~/.local/share/fonts/
 fc-cache -fv
 
 # Step 4: Verify the Installation
@@ -126,7 +141,7 @@ flatpak install flathub org.dbgate.DbGate -y
 # Wallpaper configuration
 
 # Path to the wallpaper image
-WALLPAPER="$HOME/Pictures/Wallpapers/Luffylying.png"
+WALLPAPER="~/ArchConfig/Pictures/Wallpapers/Luffylying.png"
 
 # Check if yay (AUR helper) is installed
 if ! command -v yay &> /dev/null
@@ -159,19 +174,17 @@ fi
 
 # Move application config folders to .config
 echo "Moving configuration folders to .config directory..."
-mv ./mozilla ../.config
-mv ./waybar ../.config
-mv ./wofi ../.config
-mv ./ly ../.config
-mv ./neofetch ../.config
-mv ./Documents ../
-mv ./Videos ../
-mv ./Pictures ../
+mv ~/ArchConfig/mozilla ../.config
+mv ~/ArchConfig/waybar ../.config
+mv ~/ArchConfig/wofi ../.config
+mv ~/ArchConfig/ly ../.config
+mv ~/ArchConfig/neofetch ../.config
+mv ~/ArchConfig/Pictures ../
 
 # Clean up
 echo "Cleaning up..."
 sudo pacman -Sc --noconfirm
-rm -rf ./Downloads/JetBrainsMono "$FONT_ZIP"
+rm -rf ~/Downloads/JetBrainsMono "$FONT_ZIP"
 
 echo "All done!"
 
