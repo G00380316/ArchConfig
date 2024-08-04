8f822274ab4da0a487bce74fe05b6387de54fcd8
ENOCH ABIODUN <G00380316@atu.ie>
2024-08-04 20:23:34 +0100

    notes

--- install.sh
+++ install.sh
@@ -163,7 +163,7 @@ sudo pacman -S --noconfirm htop
 # A simple, fast, and user-friendly alternative to the 'find' command. "'fd' -e" 'everthing'
 sudo pacman -S --noconfirm fd
 
-# A command-line search tool that recursively searches your current directory for a regex pattern
+# A command-line search tool that recursively searches your current directory for a regex pattern "rg"
 sudo pacman -S --noconfirm ripgrep
 
 # Clean up
