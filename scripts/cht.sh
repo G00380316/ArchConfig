df1021d802efbb2b4e8d832dc157dd6207e3c260
Enoch Abiodun <ezea128@gmail.com>
2024-12-05 00:25:58 +0000

    Added new languages to cheat sheets and disabled a defualt bind

--- scripts/cht.sh
+++ scripts/cht.sh
@@ -1,4 +1,4 @@
-languages=$(echo "shell bash golang typescript lua cpp c nodejs zsh python java ruby rust" | tr ' ' '\n')
+languages=$(echo "shell bash golang typescript lua cpp c nodejs zsh python java ruby rust dart csharp" | tr ' ' '\n')
 core_utils=$(echo "xargs find mv sed awk" | tr ' ' '\n')
 
 selected=$(printf "%s\n%s" "$languages" "$core_utils" | fzf)
